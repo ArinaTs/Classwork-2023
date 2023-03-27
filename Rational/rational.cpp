42ff535cfba322673f9cbc7ea9a2a6a18c080039
ArinaTs <GnezdoBobra@yandex.ru>
2023-03-27 21:17:39 +0300

    //

--- Rational/rational.cpp
+++ Rational/rational.cpp
@@ -11,7 +11,7 @@ protected:
 public:
 	Rational() : numerator(1), denominator(2) {}
 	Rational(int _numerator, int _denominator) : numerator(_numerator), denominator(_denominator) {}
-	Rational(const Rational& drob): numerator(drob.numerator), denominator(drob.denominator) {}
+	Rational(const Rational& drob) : numerator(drob.numerator), denominator(drob.denominator) {}
 
 	int getNumerator() {
 		return numerator;
@@ -58,7 +58,7 @@ public:
 		for (int i = 2; i <= min; i++) {
 			if (this->denominator % i == 0 && this->numerator % i == 0) {
 				res.denominator = this->denominator / i;
-			    res.numerator = this->numerator / i;
+				res.numerator = this->numerator / i;
 			}
 		}
 		return res;
@@ -76,8 +76,8 @@ class RationalWithIntPart : Rational {
 	int integer;
 public:
 	RationalWithIntPart() : integer(1) {}
-	RationalWithIntPart(int _numerator, int _denominator, int _integer): integer(_integer), Rational(_numerator, _denominator) {}
-	RationalWithIntPart(RationalWithIntPart&drob) {
+	RationalWithIntPart(int _numerator, int _denominator, int _integer) : integer(_integer), Rational(_numerator, _denominator) {}
+	RationalWithIntPart(RationalWithIntPart& drob) {
 		integer = drob.integer;
 		numerator = drob.numerator;
 		denominator = drob.denominator;
@@ -95,54 +95,64 @@ public:
 		//переводим в неправильные дроби
 		Rational convertNum1(this->numerator + this->denominator * this->integer, this->denominator);
 		Rational convertNum2(drob2.numerator + drob2.denominator * drob2.integer, drob2.denominator);
-
+		//получаем ответ в виде неправильной дроби
 		Rational resR = convertNum1.Sub(convertNum2);
-
-
+		//выделяем целую часть
 		RationalWithIntPart res(resR.getNumerator() % resR.getDenominator(),
 			resR.getDenominator(),
 			resR.getNumerator() / resR.getDenominator());
-
-		/*//вычисляем
-		res.denominator = this->denominator * drob2.denominator;
-		res.numerator = this->numerator * drob2.denominator - drob2.numerator * this->denominator;
-		//возвращаем в смешанное число
-		if (res.numerator > res.denominator) {
-			res.integer = res.numerator / res.denominator;
-			res.numerator = res.numerator % res.denominator;
-			
-		}
-		else if (res.numerator == res.denominator) {
-			res.integer = 1;
-		}
-		else {
-			return res;
-		}*/
 		return res;
 	}
 
 	RationalWithIntPart Mult(const RationalWithIntPart& drob2) {
-		RationalWithIntPart res;
 		//переводим в неправильные дроби
-		int num1 = this->numerator + this->denominator * this->integer;
-		int num2 = drob2.numerator + drob2.denominator * drob2.integer;
-		//вычисляем
-		res.denominator = this->denominator * drob2.denominator;
-		res.numerator = this->numerator * drob2.numerator;
-		//возвращаем в смешанное число
-		if (res.numerator > res.denominator) {
-			res.integer = res.numerator / res.denominator;
-			res.numerator = res.numerator % res.denominator;
+		Rational convertNum1(this->numerator + this->denominator * this->integer, this->denominator);
+		Rational convertNum2(drob2.numerator + drob2.denominator * drob2.integer, drob2.denominator);
+		//получаем ответ в виде неправильной дроби
+		Rational resR = convertNum1.Mult(convertNum2);
+		//выделяем целую часть
+		RationalWithIntPart res(resR.getNumerator() % resR.getDenominator(),
+			resR.getDenominator(),
+			resR.getNumerator() / resR.getDenominator());
+		return res;
+	}
 
-		}
-		else if (res.numerator == res.denominator) {
-			res.integer = 1;
-		}
-		else {
-			return res;
+	RationalWithIntPart Div(const RationalWithIntPart& drob2) {
+		//переводим в неправильные дроби
+		Rational convertNum1(this->numerator + this->denominator * this->integer, this->denominator);
+		Rational convertNum2(drob2.numerator + drob2.denominator * drob2.integer, drob2.denominator);
+		//получаем ответ в виде неправильной дроби
+		Rational resR = convertNum1.Div(convertNum2);
+		//выделяем целую часть
+		RationalWithIntPart res(resR.getNumerator() % resR.getDenominator(),
+			resR.getDenominator(),
+			resR.getNumerator() / resR.getDenominator());
+		return res;
+	}
+
+	RationalWithIntPart Easy() {
+		RationalWithIntPart res(this->numerator, this->denominator, this->integer);
+		int min = this->numerator;
+		if (this->denominator <= this->numerator) min = this->denominator;
+		res.denominator = this->denominator;
+		res.numerator = this->numerator;
+		for (int i = 2; i <= min; i++) {
+			if (this->denominator % i == 0 && this->numerator % i == 0) {
+				res.denominator = this->denominator / i;
+				res.numerator = this->numerator / i;
+			}
 		}
 		return res;
 	}
+
+	RationalWithIntPart Integer() {
+		//выделяем целую часть
+		RationalWithIntPart res(this->numerator % this->denominator,
+			this->denominator,
+			this->integer + this->numerator / this->denominator);
+		return res;
+	}
+
 
 	void print() {
 		std::cout << this->integer << " целых " << this->numerator << "/" << this->denominator;
@@ -152,21 +162,50 @@ public:
 
 int main() {
 	setlocale(LC_ALL, "rus");
-	/*Rational d1(3, 4), d2(3, 5), d3;
-	d3 = d2.Add(d1);
-	d3.print();*/
-	RationalWithIntPart dr1(1, 4, 2), dr2(2, 3, 1), dr3;
-	dr3 = dr2.Mult(dr1);
-	dr3.print();
+	MENU();
 }
 
-/*int NOD(int a, int b) {
-  if (a % b == 0)
-    return b;
-  if (b % a == 0)
-    return a;
-  if (a > b)
-    return NOD(a%b, b);
-  return NOD(a, b%a);
+void MENU() {
+	system("cls");
+	int n;
+	std::cout << "Выберите пункт:\n1. Действия с дробями\n2. Действия со смешанными числами\nВаш выбор: ";
+	std::cin >> n;
+	if (n == 1) {
+		return DROBI;
+	}
+	else if (n == 2) {
+		return SMESH;
+	}
+}
+
+void DROBI() {
+	system("cls");
+	int n;
+	std::cout << "Выберите пункт:\n1. Упростить дробь\n2. Другое\nВаш выбор: ";
+	std::cin >> n;
+	if (n == 1) {
+		Rational d1(a, b);
+		std::cout << "Введите дробь в формате <числитель знаменатель>";
+		std::cin >> a >> " " >> b;
+	}
+}
+
+void SMESH() {
+	system("cls");
+	int n;
+	std::cout << "Выберите пункт:\n1. Упростить\n2. Выделить целую часть\n3. Другое\nВаш выбор: ";
+	std::cin >> n;
+	if (n == 1) {
+		return DROBI
+	}
+}
+
+/*
+Rational d1(2, 8), d2(1, 7), d3;
+d3 = d1.Easy();
+d3.print();
+RationalWithIntPart dr1(7, 6, 1), dr2(2, 5, 4), dr3;
+dr3 = dr1.Integer();
+dr3.print();
 }
 */
